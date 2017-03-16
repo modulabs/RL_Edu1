7df5e0e10603e6a81ecdc8701727518e399cb83e
dhfromkorea <dhllee424@gmail.com>
2017-03-16 11:46:45 +0900

    tuned the hyperparams so that the model passes openai criteria

--- lecture/lab5/qlearning_stochastic_alpha_e_greedy.py
+++ lecture/lab5/qlearning_stochastic_alpha_e_greedy.py
@@ -6,10 +6,19 @@ import matplotlib.pyplot as plt
 import matplotlib
 import os
 
-def qlearning_alpha_e_greedy(env, n_episodes=2000, gamma=0.99, alpha=0.85):
+def qlearning_alpha_e_greedy(env, n_episodes=2000, gamma=0.99, alpha=0.85, best_enabled=False):
     nS = env.observation_space.n
     nA = env.action_space.n
     print("Q space initialized: {} x {}".format(nS, nA))
+
+    if best_enabled:
+        # record your best-tuned hyperparams here
+        env.seed(0)
+        np.random.seed(0)
+        alpha = 0.05
+        gamma = 0.99
+        epsilon_decay = 0.85
+        e = 1.0
 
     Q = np.zeros([nS, nA])
     # policy: pi(state) -> prob. distribution of actions
@@ -17,18 +26,17 @@ def qlearning_alpha_e_greedy(env, n_episodes=2000, gamma=0.99, alpha=0.85):
     reward_per_episode = np.zeros(n_episodes)
 
     for i in range(n_episodes):
-        # useful for debugging
-        if (i + 1) % 100 == 0:
-            print("\rEpisode {}/{}.".format(i + 1, n_episodes), end="")
-            sys.stdout.flush()
+        # useful for debuggin
+        log_episode(i, n_episodes)
 
         s = env.reset()
         done = False
         total_reward = 0
 
-        # extremely large variance depending on decaying speed, given n_epi=2000
-        e = 1.0 /((i//100) + 1.0)
-
+        if best_enabled:
+            e *= epsilon_decay
+        else:
+            e = 1.0 /((i/10) + 1.0)
 
         while not done:
             # Choose action by decaying e-greedy
@@ -37,13 +45,18 @@ def qlearning_alpha_e_greedy(env, n_episodes=2000, gamma=0.99, alpha=0.85):
             # take a step
             next_s, r, done, _ = env.step(a)
 
-            # backup Q
-            td_target = r + gamma * np.max(Q[next_s, :])
+            if best_enabled:
+                mod_r = modify_reward(r, done)
+                td_target = mod_r + gamma * np.max(Q[next_s, :])
+            else:
+                td_target = r + gamma * np.max(Q[next_s, :])
+
             td_delta = td_target - Q[s, a]
             Q[s, a] += alpha * td_delta
 
             s = next_s
             total_reward += r
+
 
         reward_per_episode[i] = total_reward
     return Q, reward_per_episode
@@ -62,6 +75,43 @@ def make_decay_e_greedy_policy(Q, nA):
     return policy_fn
 
 
+def modify_reward(reward, done):
+    # 100.0 being arbitrary scaling factors
+    if done and reward == 0:
+        return -100.0
+    elif done:
+        return 100.0
+    else:
+        return -1.0
+
+
+def log_episode(i_epi, n_epi):
+    if (i_epi + 1) % 100 == 0:
+        print("\rEpisode {}/{}.".format(i_epi + 1, n_epi), end="")
+        sys.stdout.flush()
+
+
+def is_solved(stats):
+    """
+    checks if openai's criteria has been met
+    """
+    CRITERIA = 0.78
+    # FrozenLake-v0 is considered "solved" when the agent
+    # obtains an average reward of at least 0.78 over 100
+    # consecutive episodes.
+
+    def moving_avg(x, n=100):
+        return np.convolve(x, np.ones((n,))/n, mode='valid')
+
+    ma = moving_avg(stats)
+    peaks = np.where(ma > CRITERIA)[0]
+    if len(peaks) > 0:
+        print("solved after {} episodes".format(peaks[0]))
+        return True
+    else:
+        print("did not pass the openai criteria")
+        return False
+
 def visualize(Q, stats, output_title="output.png"):
     print("Success rate : {}".format(np.sum(stats)/len(stats)))
     print("Final Q-Table Values")
@@ -77,9 +127,12 @@ if __name__ == "__main__":
 
     env = gym.make('FrozenLake-v0')
     env = wrappers.Monitor(env, '/tmp/frozenlake-experiment-1', force=True)
-    Q, stats = qlearning_alpha_e_greedy(env, n_episodes=100, gamma=0.5, alpha=0.5)
+    Q, stats = qlearning_alpha_e_greedy(env, best_enabled=True)
     env.close()
-    OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
-    gym.upload('/tmp/frozenlake-experiment-1', api_key=OPENAI_API_KEY)
+
+    if is_solved(stats):
+        OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+        gym.upload('/tmp/frozenlake-experiment-1', api_key=OPENAI_API_KEY)
+
     visualize(Q, stats, "qlearning_e_greedy.png")
 
