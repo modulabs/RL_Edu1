048d565ef9c2d81eb4078043d0994a96fce38e20
oasishun <oasishun@gmail.com>
2017-03-23 01:12:00 +0900

    lab6 실습용

--- lecture/lab6/qNetwork_fronzenLake.py
+++ lecture/lab6/qNetwork_fronzenLake.py
@@ -6,25 +6,20 @@ import matplotlib.pyplot as plt
 env = gym.make('FrozenLake-v0')
 
 input_size = env.observation_space.n
-hidden_size = 20
 output_size = env.action_space.n
-learning_rate = .1
 
+#learning_rate = .01  #성공률 0.103
+learning_rate = .1  #성공률 0.485
+#learning_rate = .3  #성공률 0.412
+#learning_rate = .5  #성공률 0.015
 
 def one_hot(x):
     return np.identity(input_size)[x:x+1]
 
 X = tf.placeholder(shape=[1,input_size], dtype=tf.float32)
-#W1 = tf.Variable(tf.random_uniform([input_size, hidden_size], -1, 1))
-W1 = tf.get_variable("W1",shape=[input_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer())
+W = tf.Variable(tf.random_uniform([input_size, output_size], 0, 0.01))
 
-Z1 = tf.matmul(X,W1)
-Z1 = tf.nn.tanh(Z1)
-#W2 = tf.Variable(tf.random_uniform([hidden_size, output_size], -1, 1))
-W2 = tf.get_variable("W2",shape=[hidden_size, output_size], initializer=tf.contrib.layers.xavier_initializer())
-
-
-Qpred = tf.matmul(Z1,W2)
+Qpred = tf.matmul(X,W)
 
 Y = tf.placeholder(shape=[1,output_size], dtype=tf.float32)
 loss = tf.reduce_sum(tf.square(Y - Qpred))
@@ -32,11 +27,16 @@ loss = tf.reduce_sum(tf.square(Y - Qpred))
 train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
 
 dis = .99
-num_episodes = 3000 #Percent of success: 0.604
+num_episodes = 2000
 
 rList = []
+subRewardList = []
+avgRewardList = []
+
 
 init = tf.global_variables_initializer()
+
+
 
 with tf.Session() as sess:
     sess.run(init)
@@ -47,6 +47,7 @@ with tf.Session() as sess:
         rAll = 0
         done = False
         local_loss = []
+
         count = 0
 
         while not done:
@@ -70,7 +71,18 @@ with tf.Session() as sess:
             s = s1
             count += 1
 
-        print(i,' episode =', rAll, ', count =', count)
+        subRewardList.append(rAll)
         rList.append(rAll)
 
+        if (i+1) % 100 == 0 :
+            avg = sum(subRewardList)/100
+            avgRewardList.append(avg)
+            print(i+1,' episode =', rAll, ', avg =', avg)
+            subRewardList = []
+
 print('Percent of success:', str(sum(rList) / num_episodes ) )
+
+plt.ylim(0,1)
+plt.bar(range(len(avgRewardList)), avgRewardList, color='blue', bottom=0)
+
+plt.show()
